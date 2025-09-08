cb261a6cfce99285b726c1e8077238a9ad47973a
Andy Primawan <andyprimawan@gmail.com>
2025-09-08 11:11:40 +0700

    feat(prisma): create a dedicated module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, OnModuleInit, Global } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+
+@Global()
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
